d2fe4528b8f679536d266a3820da556a114aa826
amin shahin <aminshahin.web@gmail.com>
2022-11-01 13:30:59 -0700

    add all project in git

---
+++ javascript.js
@@ -0,0 +1,24 @@
+var menu,btn = document.getElementById("menu-btn");
+var sideNav = document.getElementById("sideNav");
+sideNav.style.right = "-250px";
+menubtn.onclick = function () {
+    if(sideNav.style.right == "-250px"){
+        sideNav.style.right = "0";
+    }
+    else{
+        sideNav.style.right = "-250px";
+    }
+}
+
+var scroll = new SmoothScroll('a[href*="#"]', {
+	speed: 1500,
+	speedAsDuration: true
+});
+
+
+
+
+
+
+
+
